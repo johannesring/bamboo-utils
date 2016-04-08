29b3825dc5f4994612c1129ff183dc534467b491
Johannes Ring <johannr@simula.no>
2016-04-08 12:45:22 +0200

    Fix typo (WORKDIR -> BAMBOO_WORKDIR)

--- dockerfiles/ffc/bin/run-uflacs-unittests.sh
+++ dockerfiles/ffc/bin/run-uflacs-unittests.sh
@@ -7,4 +7,4 @@ sudo chmod -R a+w ${BAMBOO_WORKDIR}
 
 # Run tests and copy results to BAMBOO_WORKDIR
 cd ${HOME}/build/src/ffc
-${PYTHON} -B -m pytest -svl --junitxml ${WORKDIR}/report.xml test/uflacs/unit
+${PYTHON} -B -m pytest -svl --junitxml ${BAMBOO_WORKDIR}/report.xml test/uflacs/unit
