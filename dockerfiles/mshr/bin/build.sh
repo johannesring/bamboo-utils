2818f35167d84a2705eab80952b8ae12d03adb8a
Johannes Ring <johannr@simula.no>
2016-04-11 18:15:32 +0200

    Minor fixes in mshr build script

--- dockerfiles/mshr/bin/build.sh
+++ dockerfiles/mshr/bin/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 export FENICS_BUILD_TYPE=${FENICS_BUILD_TYPE:-"Developer"}
-export CMAKE_EXTRA_ARGS=${CMAKE_EXTRA_ARGS:-"-DENABLE_TESTS:BOOL=ON"}
-export VERBOSE=${VERBOSE:-1}
 export PROCS=${PROCS:-1}
 
 packages="fiat ufl instant dijitso ffc dolfin"
@@ -12,4 +10,7 @@ done
 
 # Build mshr with FENICS_BUILD_TYPE=Debug
 export FENICS_BUILD_TYPE=Debug
+export CMAKE_EXTRA_ARGS=${CMAKE_EXTRA_ARGS:-"-DENABLE_TESTS:BOOL=ON"}
+export VERBOSE=${VERBOSE:-1}
+
 fenics-build mshr
